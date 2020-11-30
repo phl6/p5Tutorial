c3e7e8ad8b8170e4a532e11afcbc74024a4b70f9
phl6 <guitarlouisli1997@gmail.com>
2020-11-30 16:11:04 +0800

    codingTrain 7.4 mouse interaction

--- empty-example/Bubble.js
+++ empty-example/Bubble.js
@@ -3,6 +3,7 @@ class Bubble{
         this.x = x;
         this.y = y;
         this.r = r;
+        this.brightness = 0;
     }
     
     show(){
@@ -10,12 +11,23 @@ class Bubble{
         strokeWeight(4);
         noFill();
         // noStroke();
-        // fill(255, 25);
+        fill(this.brightness, 100);
         ellipse(this.x, this.y, this.r*2);
     }
 
     move(){
         this.x = this.x + random(-5, 5);
         this.y = this.y + random(-5, 5);
+    }
+
+    clicked(px, py){
+        let d = dist(px, py, this.x, this.y);
+        if(d < this.r){
+            console.log('BUBBLE has been clicked');    
+        }else {
+            console.log('BUBBLE has NOT been clicked');
+        }
+        
+        (this.brightness == 0) ? this.brightness = 255 : this.brightness =0;
     }
 }
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,11 +1,18 @@
 // let bubble1;
 // let bubble2;
 let bubbles = [];
+let bubble;
 
 function setup() {
   createCanvas(1200, 800);
   background(153);
-
+  for (let i = 0; i < 5; i++){
+    let x = random(width);
+    let y = random(height);
+    let r = random(10, 50);
+    let b = new Bubble(x, y, r);
+    bubbles.push(b);
+  }
 }
 
 function draw() {
@@ -17,8 +24,20 @@ function draw() {
   }
 }
 
-function mouseDragged() {
-  let r = random(10,50);
-  let b = new Bubble(mouseX, mouseY, r);
-  bubbles.push(b);
+// function mouseDragged() {
+//   let r = random(10,50);
+//   let b = new Bubble(mouseX, mouseY, r);
+//   bubbles.push(b);
+// }
+
+// function mousePressed() {
+//   let r = random(10,50);
+//   let b = new Bubble(mouseX, mouseY, r);
+//   bubbles.push(b);
+// }
+
+function mousePressed(){
+  for (let i = 0; i < bubbles.length; i++){
+    bubbles[i].clicked(mouseX, mouseY);
+  }
 }
